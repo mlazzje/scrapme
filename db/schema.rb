833f4268bb260ee403f7ebf22dd99ddeec31e54e
Prelang Builder <builder@prelang.com>
2014-07-07 23:14:47 -0400

    Executed 'rake db:migrate' (development and test) to migrate all generated migrations which created schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140708030637) do
+ActiveRecord::Schema.define(version: 20140708030718) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -45,5 +45,38 @@ ActiveRecord::Schema.define(version: 20140708030637) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "issues", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "website_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "issues", ["website_id"], name: "index_issues_on_website_id"
+
+  create_table "issues_states", force: true do |t|
+    t.integer "issue_id"
+    t.integer "state_id"
+  end
+
+  add_index "issues_states", ["issue_id"], name: "index_issues_states_on_issue_id"
+  add_index "issues_states", ["state_id"], name: "index_issues_states_on_state_id"
+
+  create_table "states", force: true do |t|
+    t.string   "state"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "websites", force: true do |t|
+    t.string   "website"
+    t.string   "owner"
+    t.string   "manager"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
