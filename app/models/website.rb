642b07e5f49c49201b4ceb3e0740f673457ce602
Prelang Builder <builder@prelang.com>
2014-07-08 03:07:22 +0000

    Defined validation for attribute 'manager' on model 'Website'

--- app/models/website.rb
+++ app/models/website.rb
@@ -2,4 +2,5 @@ class Website < ActiveRecord::Base
   has_many :issues
   validates_formatting_of :website, using: :url
   validates_formatting_of :owner, using: :email
+  validates_formatting_of :manager, using: :email
 end
